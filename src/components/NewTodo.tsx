aad0931d91caffb8983c867006d8e350e83b7071
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-04-07 01:49:10 +0200

    TS with context api

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef } from "react";
 import classes from "./NewTodo.module.css";
+import { TodosContext } from "../store/todos-context";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
+  const todosCtx = useContext(TodosContext);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
@@ -14,7 +16,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
 
     todoTextInputRef.current!.focus();
     todoTextInputRef.current!.value = "";
